042e996925a1101cfe48860a09be84fd33f86e77
khurram-syed <khurram245@yahoo.com>
2022-07-21 12:48:21 +0100

    KSyed : added latest chromedriver for mac

--- src/main/java/com/cardetailcheck/driver/SelniumHelper.java
+++ src/main/java/com/cardetailcheck/driver/SelniumHelper.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
 import static com.cardetailcheck.driver.SeleniumDriver.getDriver;
 
 
