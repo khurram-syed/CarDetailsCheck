9d158ccb32bc8cc5c531d46a601cc73563d2029f
khurram-syed <khurram245@yahoo.com>
2022-07-24 18:40:42 +0100

    KSyed: updated the report path

--- src/test/java/com/cardetailcheck/TestRunner.java
+++ src/test/java/com/cardetailcheck/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/resources/features"},
         plugin = {"pretty","json:target/positive/cucumber.json",
-                  "html:reports/htlm/cucumber",
+                  "html:reports/htlm/cucumber.html",
                    },
          glue = {"com.cardetailcheck.steps"}
         ,tags="@searhCars1"
